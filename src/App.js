c1e2f015539b9783c05ba5b88818647eb2a3fb35
IddrisuAminu <iddrisuaminuict@gmail.com>
2022-08-18 18:53:18 +0000

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileCard from './components/ProfileCard'
 import ProfileForm from './components/ProfileForm'
 
 function App() {
-  return (
-    <div>
 
+  const [allProfiles, setAllprofiles] = useState([
+
+    {
+      firstname: "Comfort",
+      lastname: "Yelfaare",
+      email: "comfort@gmail.com",
+      phone:"0245678904"
+    }
+  ])
+
+  const updateallprofiles = (profile) => {
+    let arr = allProfiles;
+    arr.push(profile)
+    setAllprofiles([...arr])
+  };
+
+
+  return (
+    <>
+    <div className="app">
+<h1>Our profile maker</h1>
     </div>
+    <ProfileForm submit={updateallprofiles} />
+      <hr />
+      <div className="list">
+        {allProfiles.map((person, index) => (
+          <ProfileCard key={index} card={person} /> 
+        ))}
+
+
+      </div>
+      </>
   )
 }
 
